97fcf1e8841c042a478be6f4bc873211652bc620
takeshiaoyagi <takeshi.aoyagi@asia-quest.jp>
2022-08-08 17:02:27 +0900

    入力補完(モーダル内で完結) プロフィールの中身を整理

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-{{--                <livewire:autocompliete />--}}
+{{--                <livewire:autocomplete />--}}
                 <h1><b>プロジェクト登録</b></h1>
                 {{ Form::open(['route' => 'create']) }}
                 <div class="form-group">
--- src/resources/views/livewire/counter.blade.php
+++ src/resources/views/livewire/counter.blade.php
@@ -1,8 +1,8 @@
 <div xmlns:wire="http://www.w3.org/1999/xhtml">
     <p>顧客名検索</p>
-    <input type="text" wire:model.defer="word">
+
     <button type="button" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-            wire:click.prevent="openModal()">検索</button>
+            wire:click.prevent="openModal()">顧客名選択</button>
 
 
     <x-jet-modal wire:model="modalStatus">
@@ -15,51 +15,58 @@
             <div class="p-10">
                 <!-- content -->
                 <form class="w-full">
-{{--                    <div class="md:flex md:items-center mb-6">--}}
-{{--                        <div class="md:w-1/3">--}}
-{{--                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"--}}
-{{--                                   for="inline-first-name">--}}
-{{--                                名前--}}
-{{--                            </label>--}}
-{{--                        </div>--}}
-{{--                        <div class="md:w-2/3">--}}
-{{--                            <input--}}
-{{--                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"--}}
-{{--                                id="inline-first-name" type="text" value="" name="firstname" wire:model="firstname">--}}
-{{--                            @error('firstname')--}}
-{{--                            <span class="block sm:inline text-red-700">{{ $message }}</span>--}}
-{{--                            @enderror--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-                    <div>
-{{--                        <form wire:submit.prevent="render" method="GET">--}}
-{{--                            <div>--}}
-
-{{--                                <button>--}}
-{{--                                    検索--}}
-{{--                                </button>--}}
-{{--                            </div>--}}
-
-
-{{--                        </form>--}}
 
 
 
-{{--                        <div>--}}
-{{--                            @foreach ($posts as $post)--}}
-{{--                                <form method="POST" action="/modaltest">--}}
-{{--                                    <input type="submit" name={{$post->task}}>--}}
-{{--                                </form>--}}
-{{--                            @endforeach--}}
-{{--                        </div>--}}
-
+                        <input type="text" wire:model.defer="word">
+                    <button type="button" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                            wire:click.prevent="render()">検索</button><br/>
                         <form method="post" action="modaltest">
+                            <tr>
                             @foreach ($posts as $post)
-                                <td>{{ link_to_route('dashboard', $post->task, ['item' => $post->task]) }}</td>
+                                <td>{{ link_to_route('dashboard', $post->task, ['item' => $post->task]) }}</td><br/>
                             @endforeach
+                            </tr>
                         </form>
 
+                    {{--                    <div class="md:flex md:items-center mb-6">--}}
+                    {{--                        <div class="md:w-1/3">--}}
+                    {{--                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"--}}
+                    {{--                                   for="inline-first-name">--}}
+                    {{--                                名前--}}
+                    {{--                            </label>--}}
+                    {{--                        </div>--}}
+                    {{--                        <div class="md:w-2/3">--}}
+                    {{--                            <input--}}
+                    {{--                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"--}}
+                    {{--                                id="inline-first-name" type="text" value="" name="firstname" wire:model="firstname">--}}
+                    {{--                            @error('firstname')--}}
+                    {{--                            <span class="block sm:inline text-red-700">{{ $message }}</span>--}}
+                    {{--                            @enderror--}}
+                    {{--                        </div>--}}
+                    {{--                    </div>--}}
+
+                    <div>
+                        {{--                        <form wire:submit.prevent="render" method="GET">--}}
+                        {{--                            <div>--}}
+
+                        {{--                                <button>--}}
+                        {{--                                    検索--}}
+                        {{--                                </button>--}}
+                        {{--                            </div>--}}
+
+
+                        {{--                        </form>--}}
+
+
+
+                        {{--                        <div>--}}
+                        {{--                            @foreach ($posts as $post)--}}
+                        {{--                                <form method="POST" action="/modaltest">--}}
+                        {{--                                    <input type="submit" name={{$post->task}}>--}}
+                        {{--                                </form>--}}
+                        {{--                            @endforeach--}}
+                        {{--                        </div>--}}
 
 
                     </div>
